d164da27d2220c6e7287ec8e002f0979bb2f90b2
Mariano Ressio <marianoressio@gmail.com>
2023-01-09 19:57:54 -0300

    proyectos

--- P_I_Ressio_M/src/app/app.module.ts
+++ P_I_Ressio_M/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +24,9 @@ import { HysComponent } from './components/hys/hys.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HysComponent
+    HysComponent,
+    FooterComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
