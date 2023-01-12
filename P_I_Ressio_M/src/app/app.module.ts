2fe2a5bfefd31b97e4baef911b7b041bb01f98f9
Mariano Ressio <marianoressio@gmail.com>
2023-01-12 01:48:16 -0300

    login & home

--- P_I_Ressio_M/src/app/app.module.ts
+++ P_I_Ressio_M/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HysComponent } from './components/hys/hys.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HysComponent,
     FooterComponent,
-    ProyectosComponent
+    ProyectosComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
