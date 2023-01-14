c2eb94f18fe07277c2bcb5433b5c8db7af4d929f
Mariano Ressio <marianoressio@gmail.com>
2023-01-14 17:50:30 -0300

    image fixes

--- P_I_Ressio_M/src/app/components/acerca-de/edit-acerca-de.component.ts
+++ P_I_Ressio_M/src/app/components/acerca-de/edit-acerca-de.component.ts
@@ -10,27 +10,28 @@ import { PersonaService } from 'src/app/service/persona.service';
   styleUrls: ['./edit-acerca-de.component.css']
 })
 export class EditAcercaDeComponent {
-  persona: persona= null;
+  persona: persona = null;
 
-  constructor(private activatedRouter: ActivatedRoute, private personaService: PersonaService, private router: Router, public imageService: ImageService){
+  constructor(private activatedRouter: ActivatedRoute, private personaService: PersonaService, private router: Router, public imageService: ImageService) {
 
   }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     const id = this.activatedRouter.snapshot.params['id'];
     this.personaService.detail(id).subscribe(
-      data =>{
+      data => {
         this.persona = data;
-      }, err =>{
-         alert("Error al modificar");
-         this.router.navigate(['']);
+      }, err => {
+        alert("Error al modificar");
+        this.router.navigate(['']);
       }
     )
 
   }
 
-  onUpdate(): void{
+  onUpdate(): void {
     const id = this.activatedRouter.snapshot.params['id'];
+    this.persona.img = this.imageService.url
     this.personaService.update(id, this.persona).subscribe(
       data => {
         this.router.navigate(['']);
@@ -42,8 +43,10 @@ export class EditAcercaDeComponent {
 
   }
 
-  uploadImage($event:any){
-    this.imageService.uploadImage($event)
+  uploadImage($event: any) {
+    const id = this.activatedRouter.snapshot.params['id'];
+    const name= "perfil_" + id;
+    this.imageService.uploadImage($event, name)
 
   }
 }
